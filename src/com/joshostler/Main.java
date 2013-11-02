2441e82f846fa52497074ff9d880a251a42d54b3
Josh Ostler <ostlerdev@me.com>
2013-11-02 17:46:37 -0600

    initial commit

---
+++ src/com/joshostler/Main.java
@@ -0,0 +1,13 @@
+package com.joshostler;
+
+import com.joshostler.theisland.TheIsland;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TheIsland game = new TheIsland();
+	}
+}
