d14a0447914d949d6c70e90a01b1ba7c7331e85f
John H. Freeman <john@freemania.pro>
2024-01-08 22:54:45 -0500

    updated return to specify two types of exceptions
    NullPointerException and Exception

--- src/com/joshostler/Main.java
+++ src/com/joshostler/Main.java
@@ -5,14 +5,20 @@ import com.joshostler.theisland.TheIsland;
 public class Main {
 
 	/**
-	 * @param args
+	 * @param args list of string arguments, not used during main invocation on this program
 	 */
 	public static void main(String[] args) {
 		try {
 		TheIsland game = new TheIsland();
 		} catch (NullPointerException e) {
 			e.printStackTrace();
-			System.exit(0);
+			System.exit(1);//Null pointer exception specific exit
 		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			System.exit(2); //All other exceptions
+		}
+		//exit naturally with process
 	}
 }
